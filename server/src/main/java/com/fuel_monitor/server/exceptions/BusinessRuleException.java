c73dc515dc73c71a317126318c9010657e909e35
notashock <130152937+notashock@users.noreply.github.com>
2026-05-20 12:32:23 +0530

    created an global exception handler for catching the runtime business logic exceptions

--- server/src/main/java/com/fuel_monitor/server/exceptions/BusinessRuleException.java
+++ server/src/main/java/com/fuel_monitor/server/exceptions/BusinessRuleException.java
@@ -1,4 +1,7 @@
 package com.fuel_monitor.server.exceptions;
 
-public class BusinessRuleException {
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
 }
