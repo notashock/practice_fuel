4049b407e922f4a9f2380277698ad4e6cba91d92
notashock <130152937+notashock@users.noreply.github.com>
2026-05-20 16:12:48 +0530

    modified variable names such that it maps with the frontend json names

--- server/src/main/java/com/fuel_monitor/server/dtos/request/MaintenanceRecordRequest.java
+++ server/src/main/java/com/fuel_monitor/server/dtos/request/MaintenanceRecordRequest.java
@@ -1,5 +1,6 @@
 package com.fuel_monitor.server.dtos.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fuel_monitor.server.models.enums.ServiceType;
 import lombok.*;
 
@@ -9,8 +10,11 @@ import lombok.*;
 @AllArgsConstructor
 public class MaintenanceRecordRequest {
     private Long vehicleId;
+    @JsonProperty("maintenanceType")
     private ServiceType serviceType;
+    @JsonProperty("maintenanceCost")
     private Double cost;
     private String partsReplaced;
+    @JsonProperty("description")
     private String remarks;
 }
