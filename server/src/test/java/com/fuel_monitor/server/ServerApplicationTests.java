e61c15cf3f91c2722d2e7485fd0a5bcfe3487202
notashock <130152937+notashock@users.noreply.github.com>
2026-05-20 11:33:42 +0530

    initial commit

---
+++ server/src/test/java/com/fuel_monitor/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuel_monitor.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
