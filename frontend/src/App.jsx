901be8e564933d1c25d595b397b861e4aa1454bb
kalyani-cherukuri <venkatanagasaikalyani_cherukuri@srmap.edu.in>
2026-05-20 12:45:35 +0530

    added protected route and modified app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,78 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/auth/Login";
+import Unauthorized from "./pages/Unauthorized";
 
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import FleetDashboard from "./pages/fleet/FleetDashboard";
 import DriverDashboard from "./pages/driver/DriverDashboard";
 import MechanicDashboard from "./pages/mechanic/MechanicDashboard";
 
+import ProtectedRoute from "./routes/ProtectedRoute";
+
 function App() {
+
   return (
+
     <BrowserRouter>
+
       <Routes>
+
+        {/* PUBLIC */}
 
         <Route path="/" element={<Login />} />
 
         <Route
-          path="/admin/dashboard"
-          element={<AdminDashboard />}
+          path="/unauthorized"
+          element={<Unauthorized />}
         />
+
+        {/* ADMIN */}
+
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* FLEET MANAGER */}
 
         <Route
           path="/fleet/dashboard"
-          element={<FleetDashboard />}
+          element={
+            <ProtectedRoute allowedRoles={["FLEET_MANAGER"]}>
+              <FleetDashboard />
+            </ProtectedRoute>
+          }
         />
+
+        {/* DRIVER */}
 
         <Route
           path="/driver/dashboard"
-          element={<DriverDashboard />}
+          element={
+            <ProtectedRoute allowedRoles={["DRIVER"]}>
+              <DriverDashboard />
+            </ProtectedRoute>
+          }
         />
+
+        {/* MECHANIC */}
 
         <Route
           path="/mechanic/dashboard"
-          element={<MechanicDashboard />}
+          element={
+            <ProtectedRoute allowedRoles={["MECHANIC"]}>
+              <MechanicDashboard />
+            </ProtectedRoute>
+          }
         />
 
       </Routes>
+
     </BrowserRouter>
   );
 }
